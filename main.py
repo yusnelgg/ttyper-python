8acafa7410d4fe355c1233cd8fe7dc7ca8fc6a33
Yusnegg <yusnelleyvag@gmail.com>
2025-06-17 22:42:50 -0400

    refactor main.py to enhance curses setup and update welcome message

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import curses
+import time
 
 def main(stdscr):
     curses.curs_set(1)
+    curses.start_color()
+    curses.use_default_colors()
     stdscr.clear()
-    stdscr.addstr(0, 0, "¡Hola ttyper!")
-    stdscr.refresh()
-    stdscr.getch()
+    stdscr.nodelay(True)
+
+    curses.init_pair(1, curses.COLOR_GREEN, -1)
+    curses.init_pair(2, curses.COLOR_RED, -1)
+    curses.init_pair(3, curses.COLOR_WHITE, -1)
+
+    stdscr.addstr(0, 0, "🔥 Bienvenido a ttyper!")
+
 
 curses.wrapper(main)
